36dff7307fe96d66210451fa8d3f079bd8d34490
Geena Jackson <gjacks303@gmail.com>
2022-03-24 15:12:44 -0600

    Create app class with state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
 import movieData from './movieData'
+import MovieCard from './movieCard'
+import Movies from './movies'
 import React, { Component } from 'react'
 
 class App extends Component {
   constructor() {
     super()
-    this.state = { 
-
+    this.state = {
+      movieData: [movieData]
     }
   }
 
   render() {
-
+    return (
+      <main className='App'>
+        <h1>Rancid Tomatillos</h1>
+        <Movies movieData={this.state.movieData}/>
+      </main>
+    )
   }
 }
 
