6f4598fc15c8281cc47653613b3b56f00b9df513
Sam Mistele <smmstl0@gmail.com>
2022-03-25 17:45:30 -0600

    Add onclick to MovieCard component

--- src/movieCard.js
+++ src/movieCard.js
@@ -2,9 +2,12 @@ import React from 'react'
 import movieData from './movieData'
 import './movieCard.css'
 
-const MovieCard = ({ id, title, poster_path }) => {
+const MovieCard = ({ id, title, poster_path, handleChange}) => {
   return (
-    <div className='movieCard'>
+    <div
+    className='movieCard'
+    onClick={() => handleChange(id)}
+    >
       <img src={poster_path} alt={title}/>
     </div>
   )
