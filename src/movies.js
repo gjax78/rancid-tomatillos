e83aedca25ffd6a54d084456202cedcec19dcd39
Sam Mistele <smmstl0@gmail.com>
2022-03-25 17:41:37 -0600

    Add handle change prop

--- src/movies.js
+++ src/movies.js
@@ -2,17 +2,19 @@ import React from 'react'
 import MovieCard from './movieCard'
 import './movies.css'
 import movieData from './movieData'
+import MovieInfo from './MovieInfo'
 
-const Movies = ({ movieData }) => {
-  const movies = movieData.movies
-
-  const movieCards = movies.map(movie => {
+const Movies = ({ movieData, handleChange }) => {
+  // const movies = movieData.movies
+// console.log(handleChange)
+  const movieCards = movieData.map(movie => {
     return (
       <MovieCard
         title={movie.title}
         poster_path={movie.poster_path}
         id={movie.id}
         key={movie.id}
+        handleChange={handleChange}
       />
     )
   })
