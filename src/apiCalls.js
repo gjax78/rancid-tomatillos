030215aae0f602b47af89d093dac5d421d11a93f
Sam Mistele <smmstl0@gmail.com>
2022-03-26 11:37:30 -0600

    Fix single movie api filepath

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,7 +2,11 @@ const apiData = {
   allMovieData() {
     return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
     .then(response => response.json())
-    }
+  },
+  singleMovieData(id) {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+    .then(response => response.json())
+  }
 }
 
 export default apiData;
