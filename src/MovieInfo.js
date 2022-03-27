160b5e85154791bbe93f752704c0976e61a04fc9
Geena Jackson <gjacks303@gmail.com>
2022-03-27 16:47:24 -0600

    Add formatting for genre to copy Apple

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -11,21 +11,31 @@ const MovieInfo = ({ movie, showAllMovies }) => {
             src={movie.backdrop_path}
             alt={'current movie backdrop'}
           />
+          <p className='movie-tagline'>{movie.tagline}</p>
+          <p className='movie-genres-and-release-date'>{movie.genres} · {movie.release_date.split('-')[0]}</p>
+          <h2>{movie.title}</h2>
         </section>
 
 
         <section className='single-view'>
-          <img className='movie-poster' src={movie.poster_path} alt=''/>
-          <h2>{movie.title}</h2>
-          <p>{movie.release_date}</p>
-          <p>{movie.average_rating}</p>
-          <p>{movie.budget}</p>
-          <p>{movie.revenue}</p>
-          <p>{movie.runtime}</p>
-          <p>{movie.genres}</p>
-          <p>{movie.tagline}</p>
-          <p>{movie.overview}</p>
-          <button onClick={() => showAllMovies()}>BACK</button>
+          <div className='movie-info-container'>
+            <img className='movie-poster'
+              src={movie.poster_path}
+              alt={'current movie poster'}
+            />
+            <div className='movie-info'>
+              <p className='movie-release-date'>{movie.release_date}</p>
+              <p className='movie-average-rating'>{movie.average_rating}</p>
+              <p className='movie-budget'>{movie.budget}</p>
+              <p className='movie-revenue'>{movie.revenue}</p>
+              <p className='movie-runtime'>{movie.runtime}</p>
+              <button onClick={() => showAllMovies()}>BACK</button>
+            </div>
+            <div className='movie-overview-container'>
+              <h3>Overview:</h3>
+              <p className='movie-overview'>{movie.overview}</p>
+            </div>
+          </div>
         </section>
 
       </div>
