e7f991815f9817d2d43d764549a77fb7b0be61dd
Geena Jackson <gjacks303@gmail.com>
2022-03-27 19:52:47 -0600

    Attempt randomize photo

--- src/FeatureDisplay.js
+++ src/FeatureDisplay.js
@@ -5,16 +5,25 @@ class FeatureDisplay extends Component {
   // constructor() {
   //   super()
   //   this.state = {
-  //
+  //     src: []
   //   }
+  // }
+
+  // randomizeFeatureDisplay = ({ movie }) => {
+  //   const randomIndex = Math.floor(Math.random() * this.state.src.length);
+  //   const selectedPicture = movie[randomIndex]
+  //
+  //   console.log(selectedPicture)
+  //
+  //   this.setState({ src:  selectedPicture.backdrop_path})
   // }
 
   render() {
     return (
       <section className='featureDisplay'>
         <h1>Rancid Tomatillos</h1>
-        <h3>Rogue</h3>
-        <img className='featureImage'src='https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg' alt='background image for Rogue movie' />
+        <h3>Away</h3>
+        <img className='featureImage' src='https://image.tmdb.org/t/p/original//ccwPF5jN09S4Cz7u6qs3T2yKOpO.jpg' alt='background image for Away movie' />
       </section>
     )
   }
