b207c75417064997b8ede4564924b175d17ee39b
Geena Jackson <gjacks303@gmail.com>
2022-03-27 19:53:34 -0600

    Create div for sign in button

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,10 @@ import './Header.css'
 const Header = () => {
   return (
     <header className='header'>
-      <img className='tomato' src='https://thumbs.dreamstime.com/b/delicious-single-green-tomato-isolated-black-background-clipping-path-single-green-tomato-isolated-black-background-121963520.jpg' alt='green tomato' />
+      <div className='tomato'>
+        <img className='tomato' src='https://thumbs.dreamstime.com/b/delicious-single-green-tomato-isolated-black-background-clipping-path-single-green-tomato-isolated-black-background-121963520.jpg' alt='green tomato' />
+      </div>
+      <button className='sign-in-button'>Sign In</button>
     </header>
   )
 }
