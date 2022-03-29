1d8e9710e3a41d9a53a90df6b11f44ba37b2e97e
Sam Mistele <smmstl0@gmail.com>
2022-03-29 14:14:32 -0600

    Add error message display

---
+++ src/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorDisplay = ({ error }) => {
+  let newError = 'Something went wrong'
+
+  return (
+    <p>{newError}</p>
+  )
+}
+
+export default ErrorDisplay;
