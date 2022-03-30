003488b1ee28b37aec754a57798af012d3066dc0
Sam Mistele <smmstl0@gmail.com>
2022-03-29 19:14:00 -0600

    Remove second api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,19 +1,9 @@
 const apiData = {
-  allMovieData() {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/`)
+  allMovieData(url) {
+    return fetch(url)
     .then(response => {
       if(!response.ok) {
         throw new Error('Error')
-      } else {
-        return response.json()
-      }
-    })
-  },
-  singleMovieData(id) {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-    .then(response => {
-      if(!response.ok) {
-        throw new Error('Refresh browser')
       } else {
         return response.json()
       }
