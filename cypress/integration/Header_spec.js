2096c6d028d07f251c9f5847a676ba85e497f6cb
Sam Mistele <smmstl0@gmail.com>
2022-03-30 20:03:00 -0600

    Add test for sign in button display

--- cypress/integration/Header_spec.js
+++ cypress/integration/Header_spec.js
@@ -1,9 +1,13 @@
 describe("Header should render", () => {
   beforeEach(() => {
-    cy.visit("/")
+    cy.visit('/')
   })
 
   it("Should have a header", () => {
-    cy.get('header').should("be.visible")
+    cy.get('header').should('be.visible')
+  })
+
+  it("Should have a sign in button", () => {
+    cy.get('.sign-in-button').should('be.visible')
   })
 })
