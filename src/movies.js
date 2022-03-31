ef4634ba08c6fa2b928f654d9b6fab5b0f5b2a9b
Sam Mistele <smmstl0@gmail.com>
2022-03-30 18:56:02 -0600

    Refactor filename

--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import MovieCard from './movieCard'
-import './movies.css'
+import MovieCard from './MovieCard'
+import './Movies.css'
 import MovieInfo from './MovieInfo'
 
 
