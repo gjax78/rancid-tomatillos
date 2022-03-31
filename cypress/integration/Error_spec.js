456279d406a3d1b0e75dba9efd20e28706d95b8d
Sam Mistele <smmstl0@gmail.com>
2022-03-31 16:52:56 -0600

    Add test for incorrect movie id

---
+++ cypress/integration/Error_spec.js
@@ -0,0 +1,12 @@
+describe("Error handling test", () => {
+  beforeEach(() => {
+    cy.fixture('SingleMovieData.json').then((movie) => {
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/718444', movie)
+  })
+})
+
+  it('should throw error when incorrect movie id is passed', () => {
+    cy.visit('http://localhost:3000/movies/433')
+    cy.get('.error').should('have.text', 'Something went wrong')
+  })
+})
