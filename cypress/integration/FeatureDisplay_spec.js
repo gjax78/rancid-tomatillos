6cab6edbaf43169adc07bdd31d01eebca4965c3f
Sam Mistele <smmstl0@gmail.com>
2022-03-30 20:30:34 -0600

    Add test for movie title and App name

---
+++ cypress/integration/FeatureDisplay_spec.js
@@ -0,0 +1,18 @@
+describe("Header should render", () => {
+  beforeEach(() => {
+    cy.fixture('AllMovieData.json').then((movie) => {
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', movie)
+  })
+
+    cy.visit('/')
+  })
+
+  it("As a user, I should see page name Rancid Tomatillos", () => {
+    cy.get('.featureDisplay').contains('Rancid Tomatillos')
+  })
+
+  it("As a user, I should see the feature movie title", () => {
+    cy.get('h3').should('have.text', '')
+  })
+
+})
